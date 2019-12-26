b5770e4b060bc55ad1a5f8fa9c1f72bac805edc7
Dan M <djmthedev@gmail.com>
2019-12-25 20:11:26 -0600

    added PropertyKeys

---
+++ DeadOrAlive/DeadOrAlive/Helpers/PropertyKeys.swift
@@ -0,0 +1,19 @@
+//
+//  PropertyKeys.swift
+//  DeadOrAlive
+//
+//  Created by morse on 12/25/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct PropertyKeys {
+    static let easySegue = "ShowEasyTestSegue"
+    static let mediumSegue = "ShowMediumTestSegue"
+    static let hardSegue = "ShowHardTestSegue"
+    static let customSegue = "ShowCustomTestSegue"
+    static let highScoreSegue = "ShowHighScoreSegue"
+    static let loginSegue = "ShowLoginSegue"
+    static let signUpSegue = "ShowSignUpSegue"
+}
