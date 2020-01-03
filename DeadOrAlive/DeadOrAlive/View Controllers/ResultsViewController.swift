6fb5efb1b0abe9fd35ee3410643d189de8224f56
Dan M <djmthedev@gmail.com>
2020-01-03 11:03:02 -0800

    added ResultsViewController

---
+++ DeadOrAlive/DeadOrAlive/View Controllers/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.swift
+//  DeadOrAlive
+//
+//  Created by morse on 1/3/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func startNewGame(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
